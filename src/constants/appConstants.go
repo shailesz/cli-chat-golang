43171cb2d82780f1a5c015c6276a7765389454c7
shailesz <sailesh.karki@sagarmatha.edu.np>
2022-06-07 08:38:30 +0545

    updated constants

--- src/constants/appConstants.go
+++ src/constants/appConstants.go
@@ -1,5 +1,3 @@
 package constants
 
-// change this uri to your settings
-const DATABASE_URI = "postgres://shailesz:password@localhost:5432/cli-chat-golang"
 const WEBSOCKET_URI = "http://localhost:8000/socket.io/"
